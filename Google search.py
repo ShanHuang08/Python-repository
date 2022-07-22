fed5d0e967679af315481fe9b3f9568191364a3e
ShanHuang08 <109776903+ShanHuang08@users.noreply.github.com>
2022-07-22 22:46:00 +0800

    Add files via upload

--- Google search.py
+++ Google search.py
@@ -4,6 +4,7 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from openpyxl import Workbook
 
 PATH='C:/Users/Shan/Workspace2/chromedriver.exe'
 options=Options()
@@ -13,24 +14,61 @@ url='https://www.google.com'
 def Google_Mutiple_pages_title():
     keyword=input('Google搜尋關鍵字= ')
     pages=int(input('要搜尋幾頁(整數)? '))
+    
+    SheetTitle=['連結','標題']
+    wb=Workbook()
+    ws=wb.active
+    ws.append(SheetTitle)
+    
     browser=webdriver.Chrome(PATH, options=options)
     browser.get(url)
-
     browser.find_element(By.XPATH, value='//input[@name="q"]').send_keys(keyword, Keys.RETURN)
 
     i=1
     while i<=pages:
         print('page='+str(i))
         titles=browser.find_elements(By.XPATH, value='//h3[@class="LC20lb MBeuO DKV0Md"]')
+        titles_link=browser.find_elements(By.XPATH, value='//div[@class="yuRUbf"]/a')
+        # browser.find_element(By.XPATH, '//button[text()="Some text"]')
+        # cite=browser.find_elements(By.XPATH, value='//div[@class="TbwUpd NJjxre"]/cite')
+        
+        GetLink=[]
+        for link in titles_link:
+            # GetLink=[]
+            links=link.get_attribute('href')
+            GetLink.append(links)
+            # GetLink.append(link.text) #會取到<cite>text
+            # print(f'GetLink[]= {GetLink}')
+            # ws.append(GetLink)
+        
+        GetTitle=[]
         for title in titles:
-            print(title.text)
+            GetTitle.append(title.text)
+       
+        # GetTitle[]加到GetLink[]
+        k=0
+        while k<len(GetTitle):
+            GetLink.insert(1+2*k,GetTitle[k])
+            k+=1 
+
+        # 陣列分開輸出
+        m=0
+        while m<len(GetLink)/2:
+            res=[]
+            res.append(GetLink[0+2*m])
+            res.append(GetLink[1+2*m])
+            # print(res)
+            ws.append(res)
+            m+=1
+
         i+=1
         if i <= pages:
             NextPage=browser.find_element(By.XPATH, value='//span[@style="display:block;margin-left:53px"]').click()
         else:
             break
 
-    time.sleep(5)
+    wb.save('GoogleSearch.xlsx')
+    time.sleep(3)
     browser.close()
 
 def Google_Keywords_search():
