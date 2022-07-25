3d890b2aca3980d04ef66b375176875456e0e30b
ShanHuang08 <109776903+ShanHuang08@users.noreply.github.com>
2022-07-25 10:04:34 +0800

    Add files via upload

--- BingSearch.py
+++ BingSearch.py
@@ -1,6 +1,6 @@
 from ast import keyword
 from openpyxl import Workbook
-from selenium.webdriver import Chrome
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
@@ -11,14 +11,20 @@ ws=wb.create_sheet('Bing')
 ws.append(['連結','標題'])
 keywords='測'
 pages=5
-PATH='C:/Users/Shan/Workspace2/chromedriver.exe'
+# PATH='C:/Users/shan_huang/Python/chromedriver.exe'
+PATH='C:/Users/shan_huang/Python/msedgedriver.exe'
 url='https://www.bing.com/'
 # options=Options()
 # options.add_argument('--headless')
-# browser=Chrome(PATH,options=options)
-browser=Chrome(PATH)
+# browser=webdriver.Edge(PATH,options=options)
+# browser=webdriver.Chrome(PATH)
+browser=webdriver.Edge(PATH)
 browser.get(url)
 browser.find_element(By.XPATH,value='//input[@name="q"]').send_keys(keywords,Keys.RETURN)
+
+# selenium.common.exceptions.WebDriverException: Message: unknown error: cannot determine loading status
+# from unknown error: unexpected command response
+#   (Session info: chrome=103.0.5060.114)
 i=1
 while i<=pages:
     print(f'pages={i}') 
@@ -31,7 +37,12 @@ while i<=pages:
         GetContent.append(titles.text)
         # print(GetContent)
         ws.append(GetContent)
-    i+=1 
+
+    if i==1: #點擊稍後在說
+        time.sleep(1)
+        browser.find_element(By.XPATH,value='//span[@id="bnp_hfly_cta2"]').click()
+
+    i+=1
     if i<=pages:
         browser.find_element(By.XPATH,value='//a[@title="下一頁"]').click()
     else:
@@ -40,5 +51,4 @@ while i<=pages:
 wb.save('GoogleSearch.xlsx')
 time.sleep(5)
 browser.close()
-
 
