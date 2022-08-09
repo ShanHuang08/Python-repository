e05ba715abd3697095e2fe78093defcab5097eb7
ShanHuang08 <109776903+ShanHuang08@users.noreply.github.com>
2022-08-09 17:54:36 +0800

    Add files via upload

---
+++ Selenium/def_WorldPopulation.py
@@ -0,0 +1,134 @@
+#coding=utf-8
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from openpyxl import Workbook
+
+def Proccess(browser):
+    browser.get('https://www.ifreesite.com/population/')
+    Territories=browser.find_elements(By.XPATH,value='//table[@class="if_tabletd"]/tbody/tr/td/table/tbody/tr/td[@class="if_table starj taggllj"]')
+    Countries=browser.find_elements(By.XPATH,value='//table[@class="if_tabletd"]/tbody/tr/td/table/tbody/tr/td[@width="50%"]/div')
+
+    TerrList=[]
+    for terr in Territories:
+        TerrList.append(terr.text)
+    # print(len(TerrList)) #len=12
+
+    EngTerr=[]
+    ChnTerr=[]
+    for i in range(len(TerrList)//2):
+        ChnTerr.append(TerrList[0+2*i])
+        EngTerr.append(TerrList[1+2*i])
+
+    for i in range(len(ChnTerr)):
+        ChnTerr[i]=ChnTerr[i]+'('+EngTerr[i]+')'
+    # print(ChnTerr) #['非洲(AFRICA)', '亞洲(ASIA)', '歐洲(EUROPE)', '拉美和加勒比(LATIN AMERICA AND THE CARIBBEAN)', '北美(NORTHERN AMERICA)', '大洋洲(OCEANIA)']
+
+    AllList=[]
+    for country in Countries:
+        AllList.append(country.text)
+    # print(AllList)
+
+    # 刪除空陣列
+    AllList.pop(235)
+    AllList.pop(211)
+    AllList.pop(109)
+    # print(AllList) 
+
+    EngCountryList=[]
+    Population=[]
+    ChnCountryList=[]
+    for i in range(len(AllList)):
+        StringText2=AllList[i].split('|')
+        EngCountryList.append(StringText2[0])
+        # print(StringText2)
+        StringText3=StringText2[1].split('\n')
+        # print(StringText3)
+        PopulationText=StringText3[0][1:].split(',')
+        # print(PopulationText) #len=1,2,3,4
+        if len(PopulationText)==1:
+            PopulationText=PopulationText[0]
+        elif len(PopulationText)==2:
+            PopulationText=PopulationText[0]+PopulationText[1]
+        elif len(PopulationText)==3:
+            PopulationText=PopulationText[0]+PopulationText[1]+PopulationText[2]
+        elif len(PopulationText)==4:
+            PopulationText=PopulationText[0]+PopulationText[1]+PopulationText[2]+PopulationText[3]
+        # print(PopulationText) #str
+        PopulationNum=int(PopulationText)
+        Population.append(PopulationNum)
+        ChnCountryList.append(StringText3[1])
+    # print(Population)
+
+    for i in range(len(EngCountryList)):
+        EngCountryList[i]=EngCountryList[i]+'('+ChnCountryList[i]+')'
+    # print(EngCountryList[0:3])
+    return EngCountryList, Population
+
+def Dictionary(EngCountryList, Population):
+    dictList={EngCountryList[i]:Population[i] for i in range(len(Population))}
+    # print(dictList)
+    PopulationCompare=sorted(dictList.items(),key=lambda s:s[1])
+    # print(PopulationCompare)
+
+    ResultList=[]
+    for i in range(len(Population)-1,-1,-1):
+        for j in range(2):
+            ResultList.append(PopulationCompare[i][j])
+    # print(ResultList[0:6])
+    return ResultList
+
+def PopularationProgress(ResultList,ws):
+# 人口加逗號
+    b=[]
+    for i in range(len(ResultList)//2):
+        s=''.join(str(ResultList[1+2*i]))
+        b.append(s)
+    # print(b)
+
+    for i in range(len(b)):
+        length=0
+        for j in b[i]:
+            length+=1
+        # print(f'a={length}') #10
+        if length==10:
+            ResultList.pop(1+2*i)
+            ResultList.insert(1+2*i,b[i][0:1]+','+b[i][1:4]+','+b[i][4:7]+','+b[i][7:10])
+        elif length>=7 and length<=9:
+            ResultList.pop(1+2*i)
+            ResultList.insert(1+2*i,b[i][0:length-6]+','+b[i][length-6:length-3]+','+b[i][length-3:length])
+        elif length>=4 and length<=6:
+            ResultList.pop(1+2*i)
+            ResultList.insert(1+2*i,b[i][0:length-3]+','+b[i][length-3:length])
+        else:    
+            ResultList.pop(1+2*i)
+            ResultList.insert(1+2*i,b[i][0:3])
+
+    for i in range(len(ResultList)//2):
+        # print(f'NO.{i+1}. {ResultList[0+2*i]}:{ResultList[1+2*i]}')
+        res=[]
+        res.append('NO.'+str(i+1))
+        res.append(ResultList[0+2*i])
+        res.append(ResultList[1+2*i])
+        ws.append(res)
+
+
+def main():
+    wb=Workbook()
+    ws=wb.active
+    ws.title='世界人口排名'
+    ws.append(['名次','國家','人口'])
+    options=Options()
+    options.add_argument('--headless')
+    PATH='./chromedriver.exe'
+    browser=webdriver.Chrome(PATH,options=options)
+    Proccess(browser)
+    # 如何接收def 的return?
+    test=Proccess(browser)
+    print(test)
+    # Dictionary(EngCountryList, Population) #要做6次
+    # PopularationProgress(ResultList,ws) #要做6次
+    
+    # wb.save('WorldPopulation.xlsx')
+    browser.quit()
+main()
