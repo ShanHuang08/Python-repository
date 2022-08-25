6881c12625e1a74aec2db88602f291aa4e01f0d7
ShanHuang08 <109776903+ShanHuang08@users.noreply.github.com>
2022-08-25 18:37:50 +0800

    Add files via upload

--- ListPractice.py
+++ ListPractice.py
@@ -30,12 +30,25 @@ jdata.append(data)
 # print(jdata[0][121:135]) #a09
 # print(jdata[0][135:149]) #a10
 # print(jdata[0][149:166]) #a11
-# print(jdata[0][166:178]) #"a12":"新聞稿發佈新增確診數"
+# print(jdata[0][166:178-1]) #"a12":"新聞稿發佈新增確診數"
 # print(jdata[0][1:178]) #237
 # print(jdata[0][179:356]) #236
 
-for i in range(20,30):
+# for i in range(20,30):
+#     count=0
+#     if jdata[0][90*i] in ['0','1','2','3','4','5','6','7','8','9']:
+#         count+=1
+#     else:
+#         count=0
+#     print(jdata[0][1+178*i:178*(i+1)+count])
+
+for i in range(20,23):
     print(jdata[0][1+178*i:178*(i+1)])
+
+print(jdata[0][1+178*20:178*21])
+print(jdata[0][1+178*21:178*22+2])
+print(jdata[0][1+178*22+2:178*23+2])
+    
 
 
 # print(len(jdata[0])) #len=44567
