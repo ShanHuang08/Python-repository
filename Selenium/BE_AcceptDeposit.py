aa4cc023bf1f8fd1e0523b682b4a20de202137b5
ShanHuang08 <109776903+ShanHuang08@users.noreply.github.com>
2022-08-31 17:49:04 +0800

    Update BE_AcceptDeposit.py

--- Selenium/BE_AcceptDeposit.py
+++ Selenium/BE_AcceptDeposit.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 
 PATH='./chromedriver.exe'
 channel='bh'
