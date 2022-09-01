8db00f40fde0185caffef67b33fc449a33e81adf
ShanHuang08 <109776903+ShanHuang08@users.noreply.github.com>
2022-09-01 17:53:46 +0800

    Add files via upload

--- Selenium/BE_AcceptDeposit.py
+++ Selenium/BE_AcceptDeposit.py
@@ -7,13 +7,13 @@ PATH='./chromedriver.exe'
 channel='bh'
 branch='uat'
 NewAccount='stest051'
-url='https://'+channel+'-admin-'+branch'
+url='https://'+channel+'-admin-'+branch+'.paradise-soft.com.tw/'
 browser=webdriver.Chrome(PATH)
 
 browser.get(url)
 browser.maximize_window()
-browser.find_element(By.ID,value='login').send_keys('sh') 
-browser.find_element(By.ID,value='password').send_keys('sh')
+browser.find_element(By.ID,value='login').send_keys('shanbot') 
+browser.find_element(By.ID,value='password').send_keys('shan612283')
 if branch=='uat':
     browser.find_element(By.NAME,value='otp').send_keys('1', Keys.RETURN)
 elif branch=='stage':
@@ -23,7 +23,6 @@ time.sleep(1)
 # 公司入款
 browser.get(url+'v2/finance/deposit/deposit')
 browser.implicitly_wait(5)
-
 browser.find_element(By.XPATH,value='//input[@qa-input="member_login"]').send_keys(NewAccount)
 browser.find_element(By.XPATH,value='//button[@qa-button="quick-time-today"]').click()
 browser.find_element(By.XPATH,value='//*[@id="app"]/div/div[2]/div/section/div/div[1]/div[1]/form/div[2]/div[8]/div/div/label[4]/span[1]/span').click()
@@ -33,21 +32,22 @@ D_TotalResult=browser.find_element(By.XPATH,value='//span[@class="el-pagination_
 Number=D_TotalResult.text
 Number2=Number[1:].split(' ')
 # print(Number2) #['', '0', '条']
-
-for i in range(1,int(Number2[1])+1):
-    # print(f'i={i}')
-    browser.find_element(By.XPATH,value='//*[@id="app"]/div/div[2]/div/section/div/div[2]/div/div[3]/table/tbody/tr/td[12]/div/div/button[@qa-button="accept"]').click()
-    time.sleep(1)
-    browser.find_element(By.XPATH,value='/html/body/div[8]/div/div[3]/button[2]').click()
-    time.sleep(1)
-    browser.find_element(By.XPATH,value='//button[@qa-button="search"]').click()
-    time.sleep(1)
-print(Number,'公司入款OK')
+if int(Number2[1])>0:
+    for i in range(1,int(Number2[1])+1):
+        # print(f'i={i}')
+        browser.find_element(By.XPATH,value='//*[@id="app"]/div/div[2]/div/section/div/div[2]/div/div[3]/table/tbody/tr/td[12]/div/div/button[@qa-button="accept"]').click()
+        time.sleep(1)
+        browser.find_element(By.XPATH,value='/html/body/div[8]/div/div[3]/button[2]').click()
+        time.sleep(1)
+        browser.find_element(By.XPATH,value='//button[@qa-button="search"]').click()
+        time.sleep(1)
+    print(Number,'公司入款OK')
+else:
+    print(f'公司入款共{Number2[1]}條 Skip')
 
 # 在線入款
 browser.get(url+'v2/finance/deposit/web-deposit')
 browser.implicitly_wait(5)
-
 browser.find_element(By.XPATH,value='//input[@qa-input="member_login"]').send_keys(NewAccount)
 browser.find_element(By.XPATH,value='//*[@id="app"]/div/div[2]/div/section/div/div[1]/div[1]/form/div[1]/div[3]/div/div/label[3]/span[1]/span').click()
 browser.find_element(By.XPATH,value='//button[@qa-button="search"]').click()
@@ -56,25 +56,26 @@ W_TotalResult=browser.find_element(By.XPATH,value='//span[@class="el-pagination_
 Number=W_TotalResult.text
 Number2=Number[1:].split(' ')
 # print(Number2) #['', '0', '条']
+if int(Number2[1])>0:
+    for i in range(1,int(Number2[1])+1):
+        # print(f'i={i}')
+        browser.find_element(By.XPATH,value='//*[@id="app"]/div/div[2]/div/section/div/div[2]/div/div[3]/table/tbody/tr/td[17]/div/div/button[@qa-button="accept"]').click()
+        time.sleep(1)
+        if branch=='uat':
+            browser.find_element(By.XPATH,value='/html/body/div[8]/div/div[2]/div[2]/div[1]/input').send_keys('1')
+        if branch=='stage':
+            browser.find_element(By.XPATH,value='/html/body/div[8]/div/div[2]/div[2]/div[1]/input').send_keys('2')
+        browser.find_element(By.XPATH,value='/html/body/div[8]/div/div[3]/button[2]').click()
+        time.sleep(1)
+        browser.find_element(By.XPATH,value='//button[@qa-button="search"]').click()
+        time.sleep(1)
+    print(Number,'在線入款OK')
+else:
+    print(f'在線入款共{Number2[1]}條 Skip')
 
-for i in range(1,int(Number2[1])+1):
-    # print(f'i={i}')
-    browser.find_element(By.XPATH,value='//*[@id="app"]/div/div[2]/div/section/div/div[2]/div/div[3]/table/tbody/tr/td[17]/div/div/button[@qa-button="accept"]').click()
-    time.sleep(1)
-    if branch=='uat':
-        browser.find_element(By.XPATH,value='/html/body/div[8]/div/div[2]/div[2]/div[1]/input').send_keys('1')
-    if branch=='stage':
-        browser.find_element(By.XPATH,value='/html/body/div[8]/div/div[2]/div[2]/div[1]/input').send_keys('2')
-    browser.find_element(By.XPATH,value='/html/body/div[8]/div/div[3]/button[2]').click()
-    time.sleep(1)
-    browser.find_element(By.XPATH,value='//button[@qa-button="search"]').click()
-    time.sleep(1)
-print(Number,'在線入款OK')
-
-# 虛擬幣
+# 虛擬幣入款
 browser.get(url+'v2/finance/deposit/cryptocurrency')
 browser.implicitly_wait(5)
-
 browser.find_element(By.XPATH,value='//input[@qa-input="member_login"]').send_keys(NewAccount)
 browser.find_element(By.XPATH,value='//button[@qa-button="quick-time-today"]').click()
 browser.find_element(By.XPATH,value='//*[@id="app"]/div/div[2]/div/section/div/div[1]/div[1]/form/div[2]/div[8]/div/div/label[4]/span[1]').click()
@@ -84,19 +85,21 @@ C_TotalResult=browser.find_element(By.XPATH,value='//span[@class="el-pagination_
 Number=C_TotalResult.text
 Number2=Number[1:].split(' ')
 # print(Number2) #['', '0', '条']
+if int(Number2[1])>0:
+    for i in range(1,int(Number2[1])+1):
+        # print(f'i={i}')
+        browser.find_element(By.XPATH,value='//*[@id="app"]/div/div[2]/div/section/div/div[2]/div/div[3]/table/tbody/tr/td[10]/div/div/button[@qa-button="accept"]').click()
+        time.sleep(1)
+        try:
+            browser.find_element(By.XPATH,value='/html/body/div[8]/div/div[3]/button[2]').click()
+        except: 
+            browser.find_element(By.XPATH,value='/html/body/div[7]/div/div[3]/button[2]').click()
+        time.sleep(1)
+        browser.find_element(By.XPATH,value='//button[@qa-button="search"]').click()
+        time.sleep(1)
+    print(Number,'虛擬幣入款OK')
+else:
+    print(f'虛擬幣入款共{Number2[1]}條 Skip')
 
-for i in range(1,int(Number2[1])+1):
-    # print(f'i={i}')
-    browser.find_element(By.XPATH,value='//*[@id="app"]/div/div[2]/div/section/div/div[2]/div/div[3]/table/tbody/tr/td[10]/div/div/button[@qa-button="accept"]').click()
-    time.sleep(1)
-    try:
-        browser.find_element(By.XPATH,value='/html/body/div[8]/div/div[3]/button[2]').click()
-    except: 
-        browser.find_element(By.XPATH,value='/html/body/div[7]/div/div[3]/button[2]').click()
-    time.sleep(1)
-    browser.find_element(By.XPATH,value='//button[@qa-button="search"]').click()
-    time.sleep(1)
-print(Number,'虛擬幣入款OK')
-
-time.sleep(5)
+time.sleep(2)
 browser.quit()
