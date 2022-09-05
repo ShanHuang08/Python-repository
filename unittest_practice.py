6b7dee0177c1debdd3306d6b4194a91f5d6e672a
ShanHuang08 <109776903+ShanHuang08@users.noreply.github.com>
2022-09-06 00:42:54 +0800

    Add files via upload

---
+++ unittest_practice.py
@@ -0,0 +1,23 @@
+import unittest
+from selenium import webdriver
+
+class PythonMainPage(unittest.TestCase):
+    def setUp(self) -> None:
+        self.browser=webdriver.Chrome('./chromedriver.exe')
+        self.browser.get('http://www.python.org')
+    
+    def test1(self):
+        print('Test1')
+        assert True
+    def test2(self):
+        print('Test2')
+        assert False
+    def test3(self):
+        print('Test3')
+        assert True
+
+    def tearDown(self) -> None:
+        self.browser.close()
+
+if __name__ == '__main__':
+    unittest.main()
