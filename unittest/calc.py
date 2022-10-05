09b911c0832ec9ae15bdcf6ece037a7643063ffc
ShanHuang08 <109776903+ShanHuang08@users.noreply.github.com>
2022-10-05 17:17:10 +0800

    Add files via upload

---
+++ unittest/calc.py
@@ -0,0 +1,10 @@
+def add(x,y):
+    return x+y
+def substract(x,y):
+    return x-y
+def multiply(x,y):
+    return x*y
+def division(x,y):
+    if x==0 or y==0:
+        raise ValueError("Can't divided by zero")
+    return x/y
---
+++ unittest/test_calc.py
@@ -0,0 +1,27 @@
+from cgitb import reset
+import unittest
+import calc
+
+class TestCalc(unittest.TestCase):
+    def test_add(self):
+        result=calc.add(10,5)
+        self.assertEqual(result,15)
+        self.assertEqual(calc.add(10,26),36)
+
+    def test_substract(self):
+        result=calc.substract(10,5)
+        self.assertEqual(result,5)
+        self.assertEqual(calc.substract(10,16),-6)
+
+    def test_multiply(self):
+        result=calc.multiply(10,5)
+        self.assertEqual(result,50)
+        self.assertEqual(calc.multiply(10,2),20)
+
+    def test_division(self):
+        result=calc.division(10,5)
+        self.assertEqual(result,2.0)
+        self.assertEqual(calc.division(1,1),1)
+
+if __name__=='__main__':
+    unittest.main()
