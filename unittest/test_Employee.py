1ff88a2a5466cba83674cefda42c62faa8a779a0
ShanHuang08 <109776903+ShanHuang08@users.noreply.github.com>
2022-10-20 17:42:22 +0800

    Add files via upload

--- unittest/test_Employee.py
+++ unittest/test_Employee.py
@@ -2,32 +2,40 @@ import unittest
 from Employee import EmployeeData
 
 class TestEmployee(unittest.TestCase):
+    def setUp(self) -> None: #setUp最先執行
+        self.emp1=EmployeeData('Laibah','Borys',30000)
+        self.emp2=EmployeeData('Zarah','Clegg',50000)
+        # print('setUp最先執行')
+
+    def tearDown(self) -> None: #tearDown最後執行
+        pass
+        # print('tearDown最後執行')
+    
     def test_fullname(self):
-        emp1=EmployeeData('Laibah','Borys',30000)
-        emp2=EmployeeData('Zarah','Clegg',50000)
-        self.assertEqual(emp1.fullname,'Laibah Borys')
-        self.assertEqual(emp2.fullname,'Zarah Clegg')
+        self.assertEqual(self.emp1.fullname,'Laibah Borys')
+        self.assertEqual(self.emp2.fullname,'Zarah Clegg')
+
+        self.emp1.last='Corse'
+        self.emp2.last='chen'
+        self.assertEqual(self.emp1.fullname,'Laibah Corse')
+        self.assertEqual(self.emp2.fullname,'Zarah chen')
 
     def test_email(self):
-        emp1=EmployeeData('Laibah','Borys',30000)
-        emp2=EmployeeData('Zarah','Clegg',50000)
-        self.assertEqual(emp1.email,'Laibah.Borys@email.com')
-        self.assertEqual(emp2.email,'Zarah.Clegg@email.com')
+        self.assertEqual(self.emp1.email,'Laibah.Borys@email.com')
+        self.assertEqual(self.emp2.email,'Zarah.Clegg@email.com')
+
+        self.emp1.first='John'
+        self.emp2.first='stepe'
+        self.assertEqual(self.emp1.email, 'John.Borys@email.com')
+        self.assertEqual(self.emp2.email,'stepe.Clegg@email.com')
 
     def test_wage(self):
-        emp1=EmployeeData('Laibah','Borys',30000)
-        emp2=EmployeeData('Zarah','Clegg',50000)
-        self.assertEqual(emp1.wage,30000)
-        self.assertEqual(emp2.wage,50000)
+        self.assertEqual(self.emp1.wage,30000)
+        self.assertEqual(self.emp2.wage,50000)
 
     def test_wageraise(self):
-        emp1=EmployeeData('Laibah','Borys',30000)
-        emp2=EmployeeData('Zarah','Clegg',50000)
-        self.assertEqual(emp1.wageraise,31500)
-        self.assertEqual(emp2.wageraise,52500)
-
-
-
+        self.assertEqual(self.emp1.wageraise,31500)
+        self.assertEqual(self.emp2.wageraise,52500)
 
 if __name__=='__main__':
     unittest.main()
--- unittest/test_calc.py
+++ unittest/test_calc.py
@@ -1,31 +1,25 @@
-from cgitb import reset
 import unittest
+from unittest.case import _AssertRaisesContext
 import calc
 
 class TestCalc(unittest.TestCase):
     def test_add(self):
-        result=calc.add(10,5)
-        self.assertEqual(result,15)
-        self.assertEqual(calc.add(10,26),36)
-
+        self.assertEqual(calc.add(2,3),5)
+        self.assertEqual(calc.add(5,5),10)
+    
     def test_substract(self):
-        result=calc.substract(10,5)
-        self.assertEqual(result,5)
-        self.assertEqual(calc.substract(10,16),-6)
-
+        self.assertEqual(calc.substract(2,3),-1)
+        self.assertEqual(calc.substract(3,2),1)
+    
     def test_multiply(self):
-        result=calc.multiply(10,5)
-        self.assertEqual(result,50)
-        self.assertEqual(calc.multiply(10,2),20)
-
-    def test_power(self):
-        self.assertEqual(calc.power(10,2),100)
-        self.assertEqual(calc.power(1,0),1)    
+        self.assertEqual(calc.multiply(3,4),12)
+        self.assertEqual(calc.multiply(3,2),6)
 
     def test_division(self):
-        result=calc.division(10,5)
-        self.assertEqual(result,2.0)
-        self.assertEqual(calc.division(1,1),1)
+        self.assertEqual(calc.division(10,2),5)
+        self.assertEqual(calc.division(10,0),0)
+        with self.assertRaises(ValueError):
+            calc.division(10,0)
 
 if __name__=='__main__':
     unittest.main()
