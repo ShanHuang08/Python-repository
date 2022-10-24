185e71cac498c923a70fbcd2ca88bc633a98376c
ShanHuang08 <109776903+ShanHuang08@users.noreply.github.com>
2022-10-25 06:26:14 +0800

    Add files via upload

--- unittest/calc.py
+++ unittest/calc.py
@@ -12,3 +12,29 @@ def division(x,y):
     return x/y
 def power(x,y):
     return x**y
+
+class calculator():
+    def __init__(self,x,y) -> None:
+        self.x=x
+        self.y=y
+    @property
+    def add(self):
+        return self.x+self.y
+    @property
+    def substract(self):
+        return self.x-self.y
+    @property
+    def multiply(self):
+        if self.x==0 or self.y==0:
+            raise ValueError("Can't multiply zero") 
+        return self.x*self.y
+    @property
+    def divided(self):
+        if self.x==0 or self.y==0:
+            raise ValueError("Can't divided by zero") 
+        return self.x/self.y
+    @property
+    def power(self):
+        if self.x==0 or self.y==0:
+            raise ValueError
+        return self.x**self.y
