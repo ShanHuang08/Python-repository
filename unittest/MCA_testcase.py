9d4a7f186436bf1b5c8ee21eb9c18713eb227ca4
ShanHuang08 <109776903+ShanHuang08@users.noreply.github.com>
2022-10-28 15:17:41 +0800

    Add files via upload

---
+++ unittest/MCA_testcase.py
@@ -0,0 +1,42 @@
+import unittest
+import BE_MCA
+from selenium.common.exceptions import NoSuchElementException
+
+class TestCases(unittest.TestCase):
+
+    def test_MCAtest(self):
+        BE_MCA.MCABankSync()
+        assert True
+        
+if __name__=='__main__':
+    unittest.main()
+
+
+
+# F
+# ======================================================================
+# FAIL: test_MCAtest (__main__.TestCases)
+# ----------------------------------------------------------------------
+# Traceback (most recent call last):
+#   File "c:\Users\shan_huang\Python\MCA_testcase.py", line 8, in test_MCAtest
+#     with self.assertRaises(NoSuchElementException) as cm:
+# AssertionError: NoSuchElementException not raised
+
+# ----------------------------------------------------------------------
+# Ran 1 test in 76.067s
+
+# FAILED (failures=1)
+
+# E
+# ======================================================================
+# ERROR: test_MCAtest (__main__.TestCases)
+# ----------------------------------------------------------------------
+# Traceback (most recent call last):
+#   File "c:\Users\shan_huang\Python\MCA_testcase.py", line 8, in test_MCAtest
+#     if not NoSuchElementException in BE_MCA.MCABankSync():
+# TypeError: argument of type 'NoneType' is not iterable
+
+# ----------------------------------------------------------------------
+# Ran 1 test in 76.766s
+
+# FAILED (errors=1)
