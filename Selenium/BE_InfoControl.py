a9d0b156d13e9d1fd0946f8170c090523f15ac0b
ShanHuang08 <109776903+ShanHuang08@users.noreply.github.com>
2022-11-01 18:23:04 +0800

    Add files via upload

--- Selenium/BE_InfoControl.py
+++ Selenium/BE_InfoControl.py
@@ -1,9 +1,13 @@
 
-# Link files:BE_withdraw, BE_AcceptDeposit, BE_ManualDeposit, Deposit_Benefits_Setup
+# Linked files:BE_withdraw, BE_AcceptDeposit, BE_ManualDeposit, Deposit_Benefits_Setup, version.py
 
 def Webchannel():
     return 'bh'
 def Webbranch():
     return 'uat'
 def WebNewaccount():
-    return 'stest031'
+    return 'stest034'
+def BE_account():
+    return 'shanbot'
+def BE_password():
+    return 'shan612283'
---
+++ Selenium/BE_MCA_Check.py
@@ -0,0 +1,144 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from BE_InfoControl import *
+import json
+
+exclude_code=['Miles','test']
+exclude_name=['MBANK','ewqewqe']
+exclude_url=['test']
+exclude_pic=['https://squirrel-uat.paradise-soft.com.tw//brand-image/base/bank/miles.png?1666681915','https://squirrel-uat.paradise-soft.com.tw//brand-image/base/bank/test.jpeg?1666860254']
+Banks_code=[]
+Banks_name=[]
+Banks_url=[]
+Banks_picture=[]
+Banks_status=[]
+Banks_recommend=[]
+Banks_name_success=0
+Banks_name_failed=0
+Banks_url_success=0
+Banks_url_failed=0
+Banks_picture_success=0
+Banks_picture_failed=0
+Banks_status_success=0
+Banks_status_failed=0
+Banks_recommend_success=0
+Banks_recommend_failed=0
+NameFailBanks=[]
+UrlFailBanks=[]
+PicFailBanks=[]
+
+browser=webdriver.Chrome('./Chromedriver.exe')
+browser.get('https://central-web-uat.paradise-soft.com.tw/')
+browser.maximize_window()
+time.sleep(1)
+browser.find_element(By.XPATH,value='//input[@placeholder="请输入登录帐号"]').send_keys(BE_account())
+browser.find_element(By.XPATH,value='//input[@placeholder="请输入登录密码"]').send_keys(BE_password())
+browser.find_element(By.XPATH,value='//input[@placeholder="请输入OTP"]').send_keys(1)
+browser.find_element(By.XPATH,value='//*[@id="app"]/div/form/button').click()
+time.sleep(1)
+browser.get('https://central-web-uat.paradise-soft.com.tw/brand-setting/brand.bank')
+time.sleep(2)
+
+TotalBank=browser.find_element(By.XPATH,value='//div[@class="ps-pager"]/div/span[1]').text
+# print(TotalBank) #共 193 条
+BNum=TotalBank.split(' ')
+Endpage=int(BNum[1])/30+2 #8
+# print(f'Endpage={Endpage}')
+
+for i in range(1,int(Endpage)):
+    # print(f'pages={i}')
+    BankCodes=browser.find_elements(By.XPATH,value='//*[@id="app"]/div/div/div[2]/div[2]/div/div[3]/div/div[1]/div[3]/div/div[1]/div/table/tbody/tr/td[2]/div')
+    
+    # print(BankCodes.text)
+    for res in BankCodes:
+        code=res.text
+        if code not in exclude_code:
+        # if result not in ['AAA'] and result not in ['test']: bad method
+            Banks_code.append(code)
+
+    if i<int(Endpage)-1: #7-1
+        NextPage=browser.find_element(By.XPATH,value='//div[@class="ps-pager"]/div[@role="pagination"]/button[@class="btn-next"]').click()
+        time.sleep(1)
+
+browser.get('https://central-web-uat.paradise-soft.com.tw/brand-setting/brand.bank')
+browser.implicitly_wait(5)
+
+for i in range(1,int(Endpage)):
+    BankNames=browser.find_elements(By.XPATH,value='//*[@id="app"]/div/div/div[2]/div[2]/div/div[3]/div/div[1]/div[3]/div/div[1]/div/table/tbody/tr/td[4]/div')
+    BankUrls=browser.find_elements(By.XPATH,value='//*[@id="app"]/div/div/div[2]/div[2]/div/div[3]/div/div[1]/div[3]/div/div[1]/div/table/tbody/tr/td[5]/div')
+    BankPics=browser.find_elements(By.XPATH,value='//*[@id="app"]/div/div/div[2]/div[2]/div/div[3]/div/div[1]/div[3]/div/div[1]/div/table/tbody/tr/td[3]/div/img')
+    # BankStatus=browser.find_elements(By.XPATH,value='//*[@id="app"]/div/div/div[2]/div[2]/div/div[3]/div/div[1]/div[3]/div/div[1]/div/table/tbody/tr/td[7]/div/span')
+    # BanksRec=browser.find_elements(By.XPATH,value='//*[@id="app"]/div/div/div[2]/div[2]/div/div[3]/div/div[1]/div[3]/div/div[1]/div/table/tbody/tr/td[8]/div/span')
+
+    for res in BankNames:
+        name=res.text
+        if name not in exclude_name:
+            Banks_name.append(name)
+    for res in BankUrls:
+        url=res.text
+        if url not in exclude_url:
+            Banks_url.append(url)
+    for res in BankPics:
+        pic=res.get_attribute('src')
+        if pic not in exclude_pic:
+            Banks_picture.append(pic)
+
+    if i<int(Endpage)-1: #7-1
+        NextPage=browser.find_element(By.XPATH,value='//div[@class="ps-pager"]/div[@role="pagination"]/button[@class="btn-next"]').click()
+        time.sleep(1)
+
+print(f'銀行數目:{len(Banks_code)}, 名稱:{len(Banks_name)}, 網址:{len(Banks_url)}, 圖片:{len(Banks_picture)}') #len=193 扣掉無效銀行=190+1
+
+# print(len(Banks_picture))
+# print(Banks)
+
+time.sleep(4)
+browser.quit()
+
+with open('C:/Users/shan_huang/Python/Banks.json','r',encoding="utf-8") as jsonfile:
+    jdata=json.load(jsonfile)
+
+if len(Banks_name) and len(Banks_url) and len(Banks_picture) != len(jdata['Items']):
+    if len(Banks_name) != len(jdata['Items']):
+        print(f'names({len(Banks_name)}) != jdata ')
+    elif len(Banks_url) != len(jdata['Items']):
+        print(f'urls({len(Banks_url)}) != jdata ')
+    elif len(Banks_picture) != len(jdata['Items']):
+        print(f'pictures({len(Banks_picture)}) != jdata ')
+    raise ValueError('Unable to make a comparison. Lengths are not the same!')
+    
+
+for i in range(len(jdata['Items'])): #len=191
+    if jdata['Items'][i]['name'] == Banks_name[i]:
+        Banks_name_success+=1
+    else:
+        Banks_name_failed+=1
+        NameFailBanks.append(Banks_code[i])
+    
+    if jdata['Items'][i]['url'] == Banks_url[i]: #IndexError: list index out of range
+        Banks_url_success+=1
+    else:
+        Banks_url_failed+=1
+        UrlFailBanks.append(Banks_code[i])
+    
+    if jdata['Items'][i]['imgpath'] and Banks_picture[i] in ['http']: #圖片網址比對有問題
+        Banks_picture_success+=1
+    else:
+        Banks_picture_failed+=1
+        PicFailBanks.append(Banks_code[i])
+
+print('---比對結果:---')
+print(f'成功數目:銀行名稱:{Banks_name_success}, 銀行網址:{Banks_url_success}, 銀行圖片:{Banks_picture_success}')
+print(f'失敗數目:銀行名稱:{Banks_name_failed}, 銀行網址:{Banks_url_failed}, 銀行圖片:{Banks_picture_failed}')
+print()
+if len(NameFailBanks)>0:
+    print(f'失敗銀行代號(名稱):{NameFailBanks}')
+if len(UrlFailBanks)>0:
+    print(f'失敗銀行代號(網址):{UrlFailBanks}')
+if len(PicFailBanks)>0:
+    print(f'失敗銀行代號(圖片):{PicFailBanks}')
+
+
