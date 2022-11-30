ef5626f491dd048a89613d07fd7e764796e1b3ea
ShanHuang08 <109776903+ShanHuang08@users.noreply.github.com>
2022-11-30 16:42:02 +0800

    Add files via upload

--- CodeWars/CodeWars1129.py
+++ CodeWars/CodeWars1129.py
@@ -18,8 +18,23 @@ def stock_list(listOfArt, listOfCat):
     print(answer)
     return answer
 
+def stock_list2(listOfArt, listOfCat):
+    Category=[i.split() for i in listOfArt]
+    dic={x:0 for x in listOfCat}
+    for i,j in Category:
+        # print(i[0],j)
+        if i[0] in dic:
+            dic[i[0]]+=int(j)
+    result=[]
+    for i,j in dic.items():
+        # print(i,j)
+        result.append('('+i+' : '+str(j)+')')
+    answer=' - '.join(result)
+    print(answer)
+    return answer
 
 if __name__=='__main__':
     listOfArt = ["ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"] #要分開
     listOfCat=["A", "B", "C", "W"] #(A : 20) - (B : 114) - (C : 50) - (W : 0)
     stock_list(listOfArt, listOfCat)
+    stock_list2(listOfArt, listOfCat)
