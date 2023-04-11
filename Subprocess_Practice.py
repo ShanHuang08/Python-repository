070a731812054eaac524aef0b3f8a354c7c48274
ShanHuang08 <109776903+ShanHuang08@users.noreply.github.com>
2023-04-11 20:28:38 +0800

    Update Subprocess_Practice.py

--- Subprocess_Practice.py
+++ Subprocess_Practice.py
@@ -1,18 +1,51 @@
-import subprocess, os
-def cd(cmd):
-    #cmd is expected to be something like "cd [place]"
-    cmd = cmd + " && dir" # add the pwd command to run after, this will get our directory after running cd
-    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True) # run our new command
-    out = p.stdout.read()
-    err = p.stderr.read()
-    # read our output
-    if out != "":
-        # print(out)
-        print(len(out))
-        print(type(out))
-        os.chdir(out[0:len(out) - 1]) # if we did get a directory, go to there while ignoring the newline 
-    if err != "":
-        print(err) # if that directory doesn't exist, bash/sh/whatever env will complain for us, so we can just use that
+import subprocess
+
+def CheckVersion(ip, check_pwd):
+    if check_pwd.lower() == 'y':
+        pwd = input('Input Unique Password: ')
+    else:
+        pwd = 'ADMIN'
+    
+    print(ip)
+    print(pwd)
+    sumc = 'sum.exe -i '+ip+' -u ADMIN -p '+pwd+' -c ' 
+    cmd1 = sumc + 'getbiosinfo --showall'
+    cmd2 = sumc + 'getbmcinfo'
+    cmds = cmd1 + ' && ' + cmd2
+    sumproc = subprocess.run(cmds, shell=True, capture_output=True, universal_newlines=True, cwd='C:\\Users\\Stephenhuang\\sum*')
+    
+    if sumproc.stdout != '':
+        print(sumproc.stdout)
+    else:
+        print(sumproc.stderr)
+    
+    smccom = 'SMCIPMITOOL.exe '+ip+' ADMIN '+pwd+' redfish ver'
+    smcproc = subprocess.run(smccom, shell=True, capture_output=True, universal_newlines=True, cwd='C:\\Users\\Stephenhuang\\SMC*')
+
+    if smcproc.stdout != '':
+        print(smcproc.stdout)
+    else:
+        print(smcproc.stderr)
+
     return
 
-cd("cd C:\\users")
+if __name__=='__main__':
+    ip = input('ip address: ')
+    check_pwd = input('Need a Password (y/n)')
+    CheckVersion(ip, check_pwd)
+
+
+# cd("cd C:\\users")
+
+# test1=subprocess.Popen('dirr', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, cwd='D:\\')
+
+# print(test1.communicate())
+# print(test1.stdout.read())
+# print(test1.stderr.read())
+
+# test2=subprocess.run('dir ga*', shell=True, capture_output=True, universal_newlines=True, cwd='C:\\Users\\Shan')
+
+# print(test2.stdout)
+# print(test2.stderr)
+
+
