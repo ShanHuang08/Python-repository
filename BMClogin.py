f3e39319d27529eec325e9c1263280503a873780
shan <shan800208@gmail.com>
2023-04-27 19:28:14 +0800

    0427_update

--- BMClogin.py
+++ BMClogin.py
@@ -5,41 +5,79 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import Select
 
 # https://gitlab.supermicro.com/swqa/ipmi-test-automation/-/blob/master/asset/xpath/webpagexpath.py
-ip = '10.184.30.32'
+ip = '172.31.52.168'
 account='ADMIN'
 password='ADMIN'
 
 def test():
-    browser=webdriver.Chrome('chromedriver.exe')
-    browser.get('http://'+ip)
-
-    browser.find_element(By.ID,value='details-button').click()
-    browser.find_element(By.ID,value='proceed-link').click()
-    sleep(1)
-    browser.find_element(By.ID,value='usrName').send_keys(account)
-    browser.find_element(By.ID,value='pwd').send_keys(password)
-    browser.find_element(By.ID,value='login_word').click()
-    sleep(5)
-    browser.maximize_window()
-    # Launch ikvm
-    # browser.execute_script('window.scrollTo(0,800)') #Scroll down
-    # sleep(3)
-    # browser.find_element(By.ID,value='consoleImg').click()
-
-    # Remote Control Launch console
-    browser.execute_script('goPage("remote")')
-    sleep(5)
-
-    for i in range(5):
+    browsers=[]
+    for i in range(4):
+        browser=webdriver.Chrome('chromedriver.exe')
+        # browser=webdriver.Firefox(executable_path='geckodriver.exe')
+        browser.get('http://'+ip)
+        browsers.append(browser)
+        browser.find_element(By.ID,value='details-button').click()
+        browser.find_element(By.ID,value='proceed-link').click()
+        sleep(2)
+        browser.find_element(By.ID,value='usrName').send_keys(account)
+        browser.find_element(By.ID,value='pwd').send_keys(password)
+        browser.find_element(By.ID,value='login_word').click()
+        sleep(7)
+        browser.maximize_window()
+            # Launch ikvm
+        # browser.execute_script('window.scrollTo(0,800)') #Scroll down
+        # sleep(3)
+        # browser.find_element(By.ID,value='consoleImg').click()
+        # Remote Control Launch console
+        browser.execute_script('goPage("remote")')
+        sleep(5)
+    
         browser.find_element(By.XPATH, value='//button[@id="launchConsole"]').click()
+        print(f'NO.{i} times')
         sleep(3)
+    print('5 times')
+    browsers[0].find_element(By.XPATH, value='//button[@id="launchConsole"]').click()
+    print('Waiting...')
+    sleep(100)
 
-    sleep(10)
+    # browser.execute_script('doLogout()')
+    # sleep(5)
 
-    browser.execute_script('doLogout()')
-    sleep(5)
+    browser.quit()
+
+def Firefox():
+    browsers=[]
+    for i in range(4):
+        browser=webdriver.Firefox(executable_path='geckodriver.exe')
+        browser.get('http://'+ip)
+        browsers.append(browser)
+        # browser.find_element(By.ID,value='advancedButton').click()
+        # browser.find_element(By.ID,value='exceptionDialogButton-link').click()
+        sleep(2)
+        browser.find_element(By.ID,value='usrName').send_keys(account)
+        browser.find_element(By.ID,value='pwd').send_keys(password)
+        browser.find_element(By.ID,value='login_word').click()
+        sleep(7)
+        browser.maximize_window()
+
+        # Remote Control Launch console
+        browser.execute_script('goPage("remote")')
+        sleep(5)
+    
+        browser.find_element(By.XPATH, value='//button[@id="launchConsole"]').click()
+        print(f'NO.{int(i)+1} times')
+        sleep(5)
+    print('5 times')
+    browsers[0].switch_to.window(browsers[0].current_window_handle)
+    browsers[0].find_element(By.XPATH, value='//button[@id="launchConsole"]').click()
+    print('Waiting...')
+    sleep(100)
+
+    # browser.execute_script('doLogout()')
+    # sleep(5)
 
     browser.quit()
 
 if __name__=='__main__':
     test()
+    # Firefox()
