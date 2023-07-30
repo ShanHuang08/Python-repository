55ce73324959fffca9af640a8c3874aa61ec9c76
Shan Huang <shan800208@gmail.com>
2023-07-30 21:24:34 +0800

    0730_update

--- SMASH.py
+++ SMASH.py
@@ -2,18 +2,38 @@ import paramiko
 import socket
 
 BMC_IP = '10.140.171.5'
-accounts = ['Admin', 'Operator', 'User']
-pwd = '123456'
-for account in accounts:
-    print(f'Login {account}')
-    try:
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(hostname=BMC_IP, username=account, password=pwd)
-        ssh.exec_command('show')
-        ssh.close()
-        print(f'SMASH run {account} Success')
-    except paramiko.ssh_exception.SSHException as e:
-        print(f"SSHException occurred: {str(e)}")
-    except socket.gaierror as e:
-        print(f"socket.gaierror occurred: {str(e)}")
+def LDAPLogin():
+    accounts = ['Admin', 'Operator', 'User']
+    pwd = '123456'
+    for account in accounts:
+        try:
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(hostname=BMC_IP, username=account, password=pwd)
+            ssh.exec_command('show')
+            ssh.close()
+            print(f'SMASH run {account} Success')
+        except paramiko.ssh_exception.SSHException as e:
+            print(f"SSHException occurred: {str(e)}")
+        except socket.gaierror as e:
+            print(f"socket.gaierror occurred: {str(e)}")
+
+def ADLogin():
+    accounts = []
+    pwd = ''
+    for account in accounts:
+        try:
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(hostname=BMC_IP, username=account, password=pwd)
+            ssh.exec_command('show')
+            ssh.close()
+            print(f'AD account {account} Login Success')
+        except paramiko.ssh_exception.SSHException as e:
+            print(f"SSHException occurred: {str(e)}")
+        except socket.gaierror as e:
+            print(f"socket.gaierror occurred: {str(e)}")
+
+if __name__=='__main__':
+    LDAPLogin()
+    # ADLogin()
