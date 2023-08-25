2e7b93c21c638c022402ee1594fae648aa6d1402
Shan Huang <shan800208@gmail.com>
2023-08-25 23:52:31 +0800

    0825_update

--- Strings.py
+++ Strings.py
@@ -1,4 +1,5 @@
 from random import choice, randint
+import string
 al='abcdefghijklmnopqrstuvwxyz'
 digit='1234567890'
 def KeyGenerator():
@@ -25,14 +26,50 @@ def AI_Optimize():
         result += f'{sub_result}\n'
     return result
 
+def AI_StringGenerator():
+    Num = 32
+    multiple = 1
+    while Num > 5*multiple:
+        multiple+=1
+
+    result = ''
+    for Start in range(multiple):
+        if Start % 2 != 0:  # 奇數，產生英文字母
+            for _ in range(5): 
+                result += choice(string.digits)
+        else:  # 偶數，產生數字
+            for _ in range(5):
+                result += choice(string.ascii_lowercase)
+    print(result[0:Num])
 
 def StringGenerator():
-    digit = int(input("字串長度: "))
+    Num = int(input("字串長度: "))
+    Start = 1
     result = ''
-    for i in range(digit):
-        result+=choice(al)
+
+    while Num > (5 * Start)-1:    
+        if Start %2 != 0:
+            # print("Num是英文")  #0-4, 10-14
+            for i in range(5):
+                result+=choice(al)
+        else:
+            # print("Num是數字") #5-9, 15-16(沒有符合loop條件)
+            for i in range(5):
+                result+=choice(digit)
+        Start+=1
+
+    if Start %2 != 0:
+        # print("Num是英文")
+        for i in range(5-((5*Start)-Num)): 
+            result+=choice(al)
+    else:
+        # print("Num是數字")
+        for i in range(5-((5*Start)-Num)):
+            result+=choice(digit) 
+
+    # print(f"Start={Start}")
+    # print(len(result))
     print(result)
-        
 
 
 if __name__=='__main__':
