ec3e9744e3b8507eef8c9c37604450a86da644ff
shan <shan800208@gmail.com>
2023-12-04 17:21:02 +0800

    1204_update

--- Library/POSTCode.py
+++ Library/POSTCode.py
@@ -21,7 +21,7 @@ def Get_PostCode(ip, auth):
             kdata = GET(url='https://'+ip+'/redfish/v1/Managers/1/Oem/Supermicro/Snooping/', auth=auth)[-1].json()
             count+=1
             print(f"{count}. PostCode = {kdata['PostCode']}")
-            if kdata['PostCode'] in ['00']:
+            if kdata['PostCode'] in ['00', '0000']:
                 print(f"{count}. PostCode = {kdata['PostCode']}")
                 return f"{count}. PostCode = {kdata['PostCode']}"
             if count > 300 and kdata['PostCode'] not in ['00', '0000']:
