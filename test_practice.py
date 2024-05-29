bdc6181cd4ba49f25d962b1d3499be90b44fac11
shan <shan800208@gmail.com>
2024-05-29 17:45:52 +0800

    0529_update

--- test_practice.py
+++ test_practice.py
@@ -1,7 +1,6 @@
 from Library.dictionary import *
 from Library.SMCIPMITool import SMCIPMITool, SUMTool, SMCIPMITool_Internal
 from Library.Call_Method import ASCII_to_raw, Get_Dict, Email_Format, hex_to_dec, hex_to_unicode, Modify_Frus
-# from Library.Common_Func import Check_PWD
 from ssh_connect import ssh_os
 from SUT_IP import FW_Type
 
@@ -22,10 +21,7 @@ def SMC_tools():
     smc, smc_in = SMCIPMITool(ip, uni_pwd), SMCIPMITool_Internal(ip, uni_pwd)
     return smc, smc_in
 
-# lani = get_lani_id_list(ip, uni_pwd)
-# print(lani)
-
-def Search_FW_Type(types, mbd):
+def Find_FW_Num(types, mbd):
     types = types.upper()
     mbd = mbd.upper()
     possible = []
@@ -43,12 +39,25 @@ def Search_FW_Type(types, mbd):
         else: print(f"{types}\nFW num: {FW_Type[types]['info'][0]}\n{FW_Type[types]['info'][-1]}")
     except KeyError: print(f"Branch {types} is not found!")
 
+def Find_MBDs(types, mbd):
+    for key, value in FW_Type.items():
+        print(key) #key is empty
+        if isinstance(value, dict):
+            print(value['MBDs']) # 2 types: list, dict
+        elif isinstance(value, list):
+            for val in value:
+                print(val['MBDs'])
 
-        
+
+def Search_FW_Type(types, mbd):
+    if types.strip(): 
+        Find_FW_Num(types, mbd)
+    else: pass
+       
 
 if __name__=='__main__':
-    ip = '10.140.177.51'
-    uni_pwd = 'GXBGWWDHHK'
+    ip = '10.184.21.204'
+    uni_pwd = 'NLTAFRJLHJ'
     smc, smc_in = SMC_tools()
 
     # Search_FW_Type('F201MS', 'x12stl')
