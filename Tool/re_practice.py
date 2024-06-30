ba1e8864f7540d8bc99aab73dab1dd4ae80ca758
Shan Huang <shan800208@gmail.com>
2024-07-01 06:48:51 +0800

    0701_update

--- Tool/re_practice.py
+++ Tool/re_practice.py
@@ -1,6 +1,6 @@
-import re
+import re, sys
 from Library.Call_Method import ASCII_to_raw, StringGenerator, CN_Generator
-
+sys.path.append('C:\\Users\\Stephenhuang\\Python')
 
 reformat = r"^b'\<([0-9]{1,3})\>([A-Za-z]{3} [0-9 ]{2} \d{2}:\d{2}:\d{2}) ((([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])|^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])) ([\S\s]+)"
 
